e19ba2fe056f2f95f04a291a88360478d2364a2e
albertying <albertlying@gmail.com>
2022-03-27 05:50:41 -0400

    fade animation

--- main.js
+++ main.js
@@ -2,6 +2,32 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px -100px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
